1205088169f4a4de0c43a7a111856f5e8eaf409e
IrinaMuravyeva <miomir@yandex.ru>
2026-03-30 16:59:51 +0800

    feat(trackerVC): add plus button and title

--- Tracker/UI/TrackersViewCoontroller.swift
+++ Tracker/UI/TrackersViewCoontroller.swift
@@ -8,7 +8,37 @@
 import UIKit
 
 class TrackersViewController: UIViewController {
+    var titleLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad( )
+        setupNavButton()
+        setupTitle()
+    }
+}
+
+private extension TrackersViewController {
+    func setupNavButton() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "plus"),
+            style: .plain,
+            target: nil,
+            action: nil)
+    }
+    
+    func setupTitle() {
+        titleLabel = UILabel()
+        guard let titleLabel else { return }
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Трекеры"
+        titleLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        titleLabel.numberOfLines = 0
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -105)
+        ])
     }
 }
