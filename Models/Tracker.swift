97fa5bf2f87e3ceed8b2c0d2048058bccce5c74a
IrinaMuravyeva <miomir@yandex.ru>
2026-04-05 21:02:14 +0800

    (feat(models): extend  Tracker model

--- Models/Tracker.swift
+++ Models/Tracker.swift
@@ -4,12 +4,36 @@
 //
 //  Created by Irina Muravyeva on 05.04.2026.
 //
-import UIKit
+import Foundation
 
 struct Tracker {
     let id: UUID,
         name: String,
-        color: UIColor,
+        color: TrackerColor,
         emoji: String,
-        schedule: [String]
+        schedule: [TrackerSchedule],
+        type: TrackerType
+}
+
+enum TrackerColor {
+    case red, green, blue, yellow, purple, orange, pink, brown, black, white
+}
+
+enum TrackerSchedule {
+    case daysOfWeek(Set<Weekday>)
+}
+
+enum Weekday: Int {
+    case monday = 1
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+}
+
+enum TrackerType: String {
+    case habit
+    case irregular
 }
