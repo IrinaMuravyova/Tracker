d2907400541378567ce8e578a198afb665c8aa93
IrinaMuravyeva <miomir@yandex.ru>
2026-04-11 20:06:52 +0800

    fix: advansed Tracker.swift to previous commit

--- Tracker/Models/Tracker.swift
+++ Tracker/Models/Tracker.swift
@@ -66,7 +66,7 @@ enum TrackerSchedule {
     case daysOfWeek(Set<Weekday>)
 }
 
-enum Weekday: Int {
+enum Weekday: Int, CaseIterable {
     case monday = 1
     case tuesday
     case wednesday
@@ -74,6 +74,30 @@ enum Weekday: Int {
     case friday
     case saturday
     case sunday
+    
+    var title: String {
+        switch self {
+        case .monday: return "Понедельник"
+        case .tuesday: return "Вторник"
+        case .wednesday: return "Среда"
+        case .thursday: return "Четверг"
+        case .friday: return "Пятница"
+        case .saturday: return "Суббота"
+        case .sunday: return "Воскресенье"
+        }
+    }
+
+    var shortTitle: String {
+        switch self {
+        case .monday: return "Пн"
+        case .tuesday: return "Вт"
+        case .wednesday: return "Ср"
+        case .thursday: return "Чт"
+        case .friday: return "Пт"
+        case .saturday: return "Сб"
+        case .sunday: return "Вс"
+        }
+    }
 }
 
 enum TrackerType: String {
