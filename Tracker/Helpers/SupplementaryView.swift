07e320831ba28c6411b3d52025eaa3813ed85bdd
IrinaMuravyeva <miomir@yandex.ru>
2026-04-15 13:18:05 +0800

    refactor: improve readability

--- Tracker/Helpers/SupplementaryView.swift
+++ Tracker/Helpers/SupplementaryView.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 final class SupplementaryView: UICollectionReusableView {
+    // MARK: - Static constants
     static let reuseHeaderId = "SupplementaryView"
+    
+    // MARK: - Private UI
     private var titleLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
@@ -16,18 +19,12 @@ final class SupplementaryView: UICollectionReusableView {
         return label
     }()
     
+    // MARK: - Initializes
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(titleLabel)
-        
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 24),
-            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
-        ])
+        setupViews()
+        setupConstraints()
     }
 
     required init?(coder: NSCoder) {
@@ -35,7 +32,23 @@ final class SupplementaryView: UICollectionReusableView {
         fatalError("[SupplementaryView] init(coder:) has not been implemented")
     }
     
+    // MARK: - Public methods
     func configure(title: String) {
         titleLabel.text = title
+    }
+    
+    // MARK: - Private methods
+    private func setupViews() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -6)
+        ])
     }
 }
