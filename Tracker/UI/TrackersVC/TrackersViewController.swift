029b33741db0cdf073b479acbaad1a25adb8d2c4
IrinaMuravyeva <miomir@yandex.ru>
2026-05-11 20:21:52 +0800

    fix(TrackerViewController): hide filtering button if collectionView is empty

--- Tracker/UI/TrackersVC/TrackersViewController.swift
+++ Tracker/UI/TrackersVC/TrackersViewController.swift
@@ -88,6 +88,7 @@ final class TrackersViewController: UIViewController {
         
         collectionView.isHidden = isEmpty
         emptyStageView.isHidden = !isEmpty
+        filterButton.isHidden = isEmpty
     }
 }
 
