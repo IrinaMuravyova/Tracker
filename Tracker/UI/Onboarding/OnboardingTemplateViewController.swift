5809a9dd3944f80b41623214307cfa61a85c9285
IrinaMuravyeva <miomir@yandex.ru>
2026-05-13 22:39:26 +0800

    feat(onboarding): add onboarding

---
+++ Tracker/UI/Onboarding/OnboardingTemplateViewController.swift
@@ -0,0 +1,79 @@
+//
+//  OnboardingTemplateViewController.swift
+//  Tracker
+//
+//  Created by Irina Muravyeva on 13.05.2026.
+//
+
+import UIKit
+
+final class OnboardingTemplateVC: UIViewController {
+    var onNextButtonTapped: (() -> Void)?
+    
+    private lazy var backgroundImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .blackDay
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.layer.cornerRadius = 16
+        button.backgroundColor = .blackDay
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.addTarget(self, action: #selector(nextTapped), for: .touchUpInside)
+       return button
+    }()
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(backgroundImage)
+        view.addSubview(titleLabel)
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            titleLabel.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -160),
+            
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            button.heightAnchor.constraint(equalToConstant: 60),
+        ])
+    }
+    
+    @objc private func nextTapped() {
+        onNextButtonTapped?()
+    }
+    
+    // MARK: - Public methods
+    func setImage(_ image: ImageResource) {
+        backgroundImage.image = UIImage(resource: image)
+    }
+    
+    func setTitle(_ title: String) {
+        titleLabel.text = title
+    }
+}
