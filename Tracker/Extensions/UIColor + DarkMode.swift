ba5d9d97dc1367408a24510656348a55dca70643
IrinaMuravyeva <miomir@yandex.ru>
2026-05-28 13:48:17 +0800

    (test)add snapshot test

---
+++ Tracker/Extensions/UIColor + DarkMode.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor + DarkMode.swift
+//  Tracker
+//
+//  Created by Irina Muravyeva on 28.05.2026.
+//
+
+import Foundation
--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -5,4 +5,38 @@
 //  Created by Irina Muravyeva on 26.05.2026.
 //
 
-import Foundation
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackersViewControllerTests: XCTestCase {
+
+    func testTrackersViewController() {
+        let container = CoreDataContainer()
+
+        let viewModel = TrackerListViewModel(
+            container: container
+        )
+
+        let vc = TrackersViewController(
+            container: container,
+            viewModel: viewModel
+        )
+
+        let navigationController = UINavigationController(
+            rootViewController: vc
+        )
+
+
+        assertSnapshots(
+            of: navigationController,
+            as: [.image]
+        )
+//        withSnapshotTesting(record: .all) {
+//            assertSnapshots(
+//                of: navigationController,
+//                as: [.image]
+//            )
+//        }
+    }
+}
